f64f09bc2544a8e810ad77bf6dab6eda03096a46
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2024-04-16 21:44:59 +0200

    Création de la page produits

--- produits.php
+++ produits.php
@@ -1,9 +1,32 @@
 <?php
 require_once './templates/header.php';
+
 ?>
 
+
+
 <div class="container">
-    <h1>Produits</h1>
+    <h1 class="shadow-lg p-2 mb-3 mt-3 bg-body-tertiary rounded text-center">Tableau des ventes annuelles</h1>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nom</th>
+      <th scope="col">Type</th>
+      <th scope="col">Quantité</th>
+      <th scope="col">Prix</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</table>
 </div>
 
 

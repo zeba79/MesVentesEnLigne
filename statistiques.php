6cf58385b321f7f3eb65d279d51c1664022ff640
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2024-04-16 23:03:53 +0200

    Création des requêtes d'agrégation

--- statistiques.php
+++ statistiques.php
@@ -1,32 +1,76 @@
 <?php
+require_once './lib/pdo.php';
 require_once './templates/header.php';
 
-$sql = 'SELECT SUM(prix) FROM products'
+$sql = 'SELECT CEIL(SUM(quantite*prix)) FROM produits';
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$totalDesVentes = $stmt->fetch();
+
+$sql = 'SELECT CEIL(AVG(quantite*prix)) FROM produits';
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$moyenneDesVentes = $stmt->fetch();
+
+$sql = 'SELECT SUM(quantite) FROM produits';
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$nbreArticlesVendus = $stmt->fetch();
+
+$sql = 'SELECT CEIL(AVG(prix)) FROM produits';
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$prixMoyen = $stmt->fetch();
+
+$sql = 'SELECT MIN(prix) FROM produits';
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$prixMini = $stmt->fetch();
+
+$sql = 'SELECT MAX(prix) FROM produits';
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$prixMaxi = $stmt->fetch();
+
+// $sql = 'SELECT SUM(prix) FROM products'
 ?>
 <div class="container">
     <h1 class="shadow-lg p-2 mb-3 mt-3 bg-body-tertiary rounded text-center">Statistiques</h1>
     <table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nom</th>
-      <th scope="col">Type</th>
-      <th scope="col">Quantité</th>
-      <th scope="col">Prix</th>
+        <th scope="col">Moyenne des ventes</th>
+        <td><?=$moyenneDesVentes[0];?> €</td>
+    </tr>
+   <tr>
+       <th scope="col">Total des ventes</th>
+       <td><?=$totalDesVentes[0];?> €</td>
+    </tr>
+       <th scope="col">Nombre d'articles vendus</th>
+       <td><?=$nbreArticlesVendus[0];?></td>
+   <tr>
+       <th scope="col">Prix moyen</th>
+       <td><?=$prixMoyen[0];?> €</td>
+    </tr>
+   <tr>
+       <th scope="col">Prix maxi</th>
+       <td><?=$prixMaxi[0];?> €</td>
+    </tr>
+   <tr>
+       <th scope="col">Prix mini</th>
+       <td><?=$prixMini[0];?> €</td>
+
+    </tr>
+    <tr>
+      <th scope="col">Vente par type</th>
     </tr>
   </thead>
   <tbody>
-      <?php
-foreach ($produits as $key => $produit) {?>
-    <tr>
 
-<th scope="row"><?=$produit['id'];?></th>
-<td><?=$produit['nom'];?></td>
-<td><?=$produit['type'];?></td>
-<td><?=$produit['quantite'];?></td>
+    <!-- <tr>
 <td><?=$produit['prix'];?> €</td>
-</tr>
-<?php }?>
+</tr> -->
+
   </tbody>
 </table>
 </div>
